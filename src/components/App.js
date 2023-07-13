9d8e2cc2dada1f6a0f477c565977eb99ba596f47
Vansh Gupta <you@example.com>
2023-07-13 16:39:28 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [message, setMessage]=useState('');
+
+  const handleChange = event => {
+        setMessage(event.target.value);
+    
+        // console.log('value is:', event.target.value);
+      };
+      return (
+            <div>
+              <label for="message">Enter your name:</label> 
+              <br/>
+              <br/>
+              <input
+                type="text"
+                id="message"
+                name="message"
+                onChange={handleChange}
+                value={message}
+              />
+        
+              <h2>{message}</h2>
+            </div>
+          );
+
+};
 
 export default App
+
+
